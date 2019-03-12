9f5ee1742a2c802e3867d40eb2d56d501b64b3e2
mridulmanocha <mridul.manocha22@gmail.com>
2019-03-13 00:40:55 +0530

    start command in package.json and heroku setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
  const express = require('express');
  const hbs = require('hbs');
  const fs = require('fs');
-
+ const port = process.env.PORT || 3000;
  var app = express();
 
  app.set('view engine' , 'hbs');
@@ -23,9 +23,9 @@
  	next();
  })
 
- app.use((req, res, next) => {
- 	res.render('maintenance.hbs');
- });
+ // app.use((req, res, next) => {
+ // 	res.render('maintenance.hbs');
+ // });
 
  app.use(express.static( __dirname + '/public_html'));
 
@@ -50,6 +50,6 @@ app.get('/about', (req,res) => {
  	});
  
  });
- app.listen(3000, () => {
- 	console.log('server is up and running on port 3000');
+ app.listen(port, () => {
+ 	console.log(`server is up and running on port ${port}`);
  });
